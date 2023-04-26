dd3c6c6e5ff3b8d48ecf0c392e457a8f607f863f
Martincsek Levente <martincsekl@gmail.com>
2023-04-27 01:38:26 +0200

    Added support for packaging

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="todo_telegram_chatbot",
-    version="1.0",
-    packages=find_packages(),
+    version="1.0.0",
+    author='Martincsek Levente',
+    readme="README.md",
+    author_email='martincsekl@gmail.com',
+    packages=['telegramBot'],
     install_requires=[
         "python-telegram-bot",
         "datetime",
